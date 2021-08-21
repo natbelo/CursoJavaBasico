2c4c6645685ba1a2718c21a4be5b35cf2d469f4b
de Oliveira <n.de.oliveira@accenture.com>
2021-08-21 13:00:46 -0300

    Matriz Irregular

--- curso-java-basico/src/com/natalia/cursojava/aula20/Matrizes.java
+++ curso-java-basico/src/com/natalia/cursojava/aula20/Matrizes.java
@@ -4,7 +4,7 @@ public class Matrizes {
 
 	public static void main(String[] args) {
 		
-		double[][] notasAlunos = new double[30][4];
+		double[][] notasAlunos = new double[3][4];
 		
 		notasAlunos[0][0] = 10;
 		notasAlunos[0][1] = 7;
@@ -20,6 +20,49 @@ public class Matrizes {
 		notasAlunos[2][1] = 4;
 		notasAlunos[2][2] = 9;
 		notasAlunos[2][3] = 8;
+		
+		for (int i=0; i<notasAlunos.length; i++) {
+			for (int j=0; j<notasAlunos.length; j++) {
+				System.out.print(notasAlunos[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		notasAlunos[1][3] = 2;
+		
+		System.out.println();
+		
+		for (int i=0; i<notasAlunos.length; i++) {
+			for (int j=0; j<notasAlunos.length; j++) {
+				System.out.print(notasAlunos[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Calculando a média de cada aluno");
+		
+		double soma;
+		for (int i=0; i<notasAlunos.length; i++) {
+			
+			soma = 0;
+			for (int j=0; j<notasAlunos[i].length; j++) {
+				soma += notasAlunos[i][j];
+			}
+			System.out.println("Média do aluno " + i + "é = " + (soma/4));
+		}
+		
+		double[] notasAluno1 = {7,8,9,4};
+		
+		double[][] notasAlunos2 = {{7,8,9,4},{8,6,5,7}};
+		
+		System.out.println("Output da matriz notasAlunos2");
+		
+		for (int i=0; i<notasAlunos2.length; i++) {
+			for (int j=0; j<notasAlunos2[i].length; j++) {
+				System.out.print(notasAlunos2[i][j] + "-");
+			}
+			System.out.println();
+		}
 	}
 
 }
